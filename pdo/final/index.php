e9b7f1bad200d6466ad9440b36bf016bcd2df993
bpineda <_@bernardopineda.mx>
2016-09-05 10:21:14 -0700

    Included pdo final files

---
+++ pdo/final/index.php
@@ -0,0 +1,58 @@
+<?php
+//$dsn = "mysql:host=localhost;dbname=intermediate_php;charset=utf8";
+$dsn = "sqlite:intermediate_php.sqlite";
+$pdo = new PDO($dsn, 'root', 'root');
+$stmt = $pdo->query('SELECT * FROM agents');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Curso PHP Intermedio</title>
+    <link href=
+    "http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css" rel=
+    "stylesheet">
+    <link href=
+    'http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css'
+    rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="row">
+        <div class="medium-5 large-3 columns"></div>
+    </div>
+    <div class="row column">
+        <h4>PDO</h4>
+    </div>
+    <div class="row small-up-1 medium-up-2 large-up-3">
+
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+            </tr>
+          </thead>
+          <tbody>
+          <?php foreach($stmt as $row){ ?>
+            <tr>
+              <td><?php echo $row['id']; ?></td>
+              <td><?php echo $row['name']; ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js">
+    </script> 
+    <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js">
+    </script> 
+    <script>
+      $(document).foundation();
+    </script> 
+    <script src="https://intercom.zurb.com/scripts/zcom.js" type=
+    "text/javascript">
+    </script>
+</body>
+</html>
