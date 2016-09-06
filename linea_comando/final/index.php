844d6c997ff65bc7a111d00126b40caebb8c2885
bpineda <_@bernardopineda.mx>
2016-09-06 09:49:55 -0700

    Included command line final files

---
+++ linea_comando/final/index.php
@@ -0,0 +1,9 @@
+<?php
+$people[] = [ 'name' => 'Bernardo', 'age' => 35 ];
+$people[] = [ 'name' => 'Francisco', 'age' => 29 ];
+$people[] = [ 'name' => 'Marco', 'age' => 26 ];
+echo 'PHP CLI
+';
+//phpinfo();
+//var_dump($argv);
+echo $people[ $argv[1] ][ $argv[2] ];
