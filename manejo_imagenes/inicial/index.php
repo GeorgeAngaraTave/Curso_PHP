f99407e5e32b3e1ac2d9b27212cdce97b67159d9
bpineda <_@bernardopineda.mx>
2016-09-06 13:10:25 -0700

    Added image handling original files

---
+++ manejo_imagenes/inicial/index.php
@@ -0,0 +1,70 @@
+<?php
+$action = isset( $_GET['action'] ) ? $_GET['action'] : FALSE;
+
+if( $action === 'render' )
+{
+
+    $image_file = 'house.jpg';
+
+    header('Content-Type: image/jpeg');
+
+    $image = imagecreatefromjpeg($image_file);
+    imagejpeg($image, null, 100);
+
+
+    die('');
+
+}
+
+?><!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Curso PHP Intermedio</title>
+    <link href=
+    "http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css" rel=
+    "stylesheet">
+    <link href=
+    'http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css'
+    rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="row">
+        <div class="medium-5 large-3 columns"></div>
+    </div>
+    <div class="row column">
+        <p class="lead">Manejo de imágenes</p>
+    </div>
+    <div class="row small-up-1 medium-up-2 large-up-3">
+
+        <table>
+          <thead>
+            <tr>
+              <th width="200">Acción</th>
+              <th>Encabezado</th>
+              <th width="150">Resultado</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a href="./index.php?action=render">Visualización</a></td>
+              <td>Content-type: image/format</td>
+              <td>Muestra imagen</td>
+            </tr>
+          </tbody>
+        </table>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js">
+    </script> 
+    <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js">
+    </script> 
+    <script>
+      $(document).foundation();
+    </script> 
+    <script src="https://intercom.zurb.com/scripts/zcom.js" type=
+    "text/javascript">
+    </script>
+</body>
+</html>
