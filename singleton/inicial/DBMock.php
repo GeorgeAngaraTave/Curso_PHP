41a3896d8335ac6d2af9b62993580acdc3cfac60
bpineda <_@bernardopineda.mx>
2016-09-07 08:28:59 -0700

    init files

--- singleton/inicial/DBMock.php
+++ singleton/inicial/DBMock.php
@@ -1,32 +1,9 @@
 <?php
 class DBMock
-{ 
-  private static $instance;
-  protected function __construct()
+{
+  public function __construct()
   {
-    echo 'Connecting to a DB...
+    echo 'Connecting to a DB
 ';
   }
-  public static function getInstance()
-    {
-        if ( static::$instance === null) {
-            static::$instance = new static();
-            echo 'Creando Nueva instancia
-';
-        }
-        
-        return static::$instance;
-    }
-
 }
-
-class DBEngine extends DBMock
-  {
-    
-  }
-  
-  $instanceDBEngine = DBMock::getInstance();
-  var_dump($instanceDBEngine === DBMock::getInstance());             // bool(true)
-
-  $instanceDBEngine_2 = DBMock::getInstance();
-  var_dump($instanceDBEngine_2 === DBMock::getInstance());
